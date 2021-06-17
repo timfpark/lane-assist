4eb18626115304062d9d9877f0167d625d295f21
Tim Park <timfpark@gmail.com>
2021-06-17 10:17:59 -0700

    Reserve a random amount of memory each loop

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,48 @@
-use std::{f64::consts::PI};
-
+use rand::Rng;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 use std::thread;
 
 fn main() -> std::io::Result<()> {
 
-    let hundred_millis = Duration::from_millis(100);
-    let period: f64 = 30.0; // seconds
-    let scale_factor = period / PI;
-    let base_speed = 60.0;
-    let max_change = 10.0;
+    let loop_duration_target = Duration::from_millis(100);
+    let mut rng = rand::thread_rng();
+
+    // const MAX_ITERATIONS: i32 = 10000;
+    const MAX_STRING_COPIES: i32 = 40000;
+
+    let mut strings;
+
     loop {
         let start = SystemTime::now();
-        let since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+        let start_since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
 
-        let scaled = since_the_epoch.as_secs_f64() / scale_factor;
-        let speed = base_speed + max_change * scaled.sin();
+        /*
+        let iterations = rng.gen_range(0..MAX_ITERATIONS);
 
-        println!("speed {}", speed);
+        // burn a random amount of CPU
+        let mut total: u64= 0;
+        for _ in 1..iterations {
+            total += rng.gen_range(0..50);
+        }
 
-        thread::sleep(hundred_millis);
+        println!("total is {}", total);
+        */
+
+        // reserve a random amount of memory (also burning random amounts of CPU in the process)
+        let string_copies = rng.gen_range(0..MAX_STRING_COPIES);
+        strings = Vec::new();
+        for _ in 1..string_copies {
+            strings.push(String::from("This is a string that we are adding to the vector to consume memory."));
+        }
+
+        let finish = SystemTime::now();
+        let finish_since_the_epoch = finish.duration_since(UNIX_EPOCH).expect("Time went backwards");
+
+        let consumed_time = finish_since_the_epoch - start_since_the_epoch;
+        let sleep_time = loop_duration_target - consumed_time;
+
+        println!("sleeping for {}ms", sleep_time.as_millis());
+
+        thread::sleep(sleep_time);
     }
 }
